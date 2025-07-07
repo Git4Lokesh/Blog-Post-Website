e8ea182e2c7537310f91ca38d9bce85c3ca34fdd
Lokesh Sathish <f20220732@hyderabad.bits-pilani.ac.in>
2025-07-07 13:03:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,76 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import session from "express-session";
 
 const app = express();
 const port = 3000;
-var currentuser;
+
 var userCredentials = {"dummy":{password:"dummy", email:"dummy@gmail.com",fname:"dum",lname:"my"}};
 var posts = [];
+
 app.set('view engine','ejs');
 app.set('views','./views');
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.listen(port,()=>{
-    console.log("Port is listening ");
-});
+// Session middleware
+app.use(session({
+    secret: 'your-secret-key-here', // Change this to a random string
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+        secure: false, // Set to true in production with HTTPS
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+}));
+
+// Helper function to get current username from userCredentials
+function getCurrentUsername(sessionUser) {
+    for (let username in userCredentials) {
+        if (userCredentials[username].fname === sessionUser) {
+            return username;
+        }
+    }
+    return null;
+}
+
+// Middleware to check authentication
+function requireAuth(req, res, next) {
+    if (!req.session.currentuser) {
+        return res.redirect("/");
+    }
+    next();
+}
 
 app.get("/",(req,res)=>{
-    res.render("login.ejs");
+    res.render("login.ejs", {showAlert: false, error: null});
 });
+
 app.get("/signup",(req,res)=>{
-    res.render("signup.ejs");
+    res.render("signup.ejs", {showAlert: false, error: null});
 });
+
 app.post("/login",(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
     if(username in userCredentials) {
         if(userCredentials[username].password === password) {
-            currentuser = userCredentials[username].fname; // Add this line!
-            res.render("home.ejs", {fname:currentuser, posts:posts});
+            // Store user info in session
+            req.session.currentuser = userCredentials[username].fname;
+            req.session.username = username;
+            res.render("home.ejs", {
+                fname: req.session.currentuser,
+                posts: posts,
+                showDeleteSuccess: false
+            });
         } else {
             res.render("login.ejs",{showAlert:true,error:"Wrong password"});
         }
     } else {
-        res.render("login.ejs",{showAlert:true,error:"No such user exists, please sign up. "});
+        res.render("login.ejs",{showAlert:true,error:"No such user exists, please sign up."});
     }
 });
-
 
 app.post("/signup",(req,res)=>{
     const username = req.body.username;
@@ -45,81 +78,125 @@ app.post("/signup",(req,res)=>{
     const email = req.body.email;
     const fname = req.body.fname;
     const lname = req.body.lname;
-    currentuser = fname;
-    userCredentials[username] = {password:password,
-        email:email,fname:fname,lname:lname
+    // Store in session
+    req.session.currentuser = fname;
+    req.session.username = username;
+    userCredentials[username] = {
+        password:password,
+        email:email,
+        fname:fname,
+        lname:lname
     }
-    res.render("home.ejs",{fname:currentuser,posts:posts});
+    res.render("home.ejs",{
+        fname: req.session.currentuser,
+        posts: posts,
+        showDeleteSuccess: false
+    });
 });
 
-app.get("/create",(req,res)=>{
-    res.render("create.ejs");
-})
-app.get("/home",(req,res)=>{
-    res.render("home.ejs",{fname:currentuser,posts:posts});
-})
+app.get("/create", requireAuth, (req,res)=>{
+    res.render("create.ejs", {showSuccess: false});
+});
 
+app.get("/home", requireAuth, (req,res)=>{
+    res.render("home.ejs",{
+        fname: req.session.currentuser,
+        posts: posts,
+        showDeleteSuccess: false
+    });
+});
 
-app.post("/create",(req,res)=>{
+app.post("/create", requireAuth, (req,res)=>{
     const title = req.body.title;
-    const content = req.body.content;  // Fixed
-    const author = req.body.author;    // Fixed
+    const content = req.body.content;
+    const author = req.body.author;
     var today = new Date();
     posts.push({
-        title:title,
-        content:content,
-        author:author,
-        time:today
+        title: title,
+        content: content,
+        author: author,
+        time: today,
+        owner: req.session.currentuser,
+        ownerUsername: req.session.username
     });
     res.render("create.ejs",{showSuccess:true});
 });
 
-
-app.post("/delete/:index", (req, res) => {
+app.post("/delete/:index", requireAuth, (req, res) => {
     const index = parseInt(req.params.index);
-    
     if (index >= 0 && index < posts.length) {
-        posts.splice(index, 1); // Remove 1 element at the specified index
-        // Pass showDeleteSuccess flag
-        res.render("home.ejs", {
-            fname: currentuser, 
-            posts: posts, 
-            showDeleteSuccess: true
-        });
+        const post = posts[index];
+        if (post.owner === req.session.currentuser) {
+            posts.splice(index, 1);
+            res.render("home.ejs", {
+                fname: req.session.currentuser,
+                posts: posts,
+                showDeleteSuccess: true
+            });
+        } else {
+            res.render("home.ejs", {
+                fname: req.session.currentuser,
+                posts: posts,
+                showDeleteSuccess: false,
+                error: "Unauthorized: You can only delete your own posts"
+            });
+        }
     } else {
-        // If deletion failed, don't show popup
         res.render("home.ejs", {
-            fname: currentuser, 
-            posts: posts, 
+            fname: req.session.currentuser,
+            posts: posts,
             showDeleteSuccess: false
         });
     }
 });
 
-app.get("/edit/:index",(req,res)=>{
+app.get("/edit/:index", requireAuth, (req,res)=>{
     const index = parseInt(req.params.index);
-    
-    // Add validation
     if (index >= 0 && index < posts.length) {
-        res.render("edit.ejs",{
-            post:posts[index],
-            index:index
-        });
+        const post = posts[index];
+        if (post.owner === req.session.currentuser) {
+            res.render("edit.ejs",{
+                post: posts[index],
+                index: index
+            });
+        } else {
+            res.redirect("/home");
+        }
     } else {
-        res.redirect("/home"); // Handle invalid index
+        res.redirect("/home");
     }
 });
 
-
-app.post("/edit/:index",(req,res)=>{
+app.post("/edit/:index", requireAuth, (req,res)=>{
     const index = parseInt(req.params.index);
-    const newtitle = req.body.title;
-    const newText = req.body.content;
-    var today = new Date();
-    posts[index].title = newtitle;
-    posts[index].content = newText;
-    posts[index].time = today;
-    res.redirect("/home")
-})
+    if (index >= 0 && index < posts.length) {
+        const post = posts[index];
+        if (post.owner === req.session.currentuser) {
+            const newtitle = req.body.title;
+            const newText = req.body.content;
+            var today = new Date();
+            posts[index].title = newtitle;
+            posts[index].content = newText;
+            posts[index].time = today;
+            res.redirect("/home");
+        } else {
+            res.redirect("/home");
+        }
+    } else {
+        res.redirect("/home");
+    }
+});
 
+// Logout route
+app.post("/logout", requireAuth, (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect("/");
+    });
+});
 
+app.listen(port,()=>{
+    console.log("Port is listening ");
+});
